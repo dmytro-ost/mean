736205789088fd0f0ac3554e1dff475ad6df500b
Dmytro Ostretsov <1654045@gmail.com>
2024-01-20 16:32:23 +0200

    Add cors

--- backend/index.js
+++ backend/index.js
@@ -2,11 +2,13 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const morgan = require('mongoose-morgan');
+const cors = require('cors');
 
 const app = express();
 
 const PORT = process.env.PORT || 8080;
 
+app.use(cors());
 app.use(express.json());
 
 app.use(
